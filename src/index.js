8f09b69216f2edcd39fc137499a703486b804e0a
Patrick Michelberger <patrick@michelberger.info>
2017-01-13 15:01:10 +0100

    add speech assets

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const Alexa = require('alexa-sdk');
 const speechOutput = {
     "SKILL_NAME": "Hitch",
     "WELCOME_MESSAGE": "Welcome to hitch.",
-    "HELP_MESSAGE": "You can start hitch by saying 'start hitch'",
+    "HELP_MESSAGE": "You can start hitch by saying ' '",
     "HELP_REPROMPT": "How can I help you?",
     "STOP_MESSAGE": "See you!"
 };
@@ -13,7 +13,6 @@ const speechOutput = {
 exports.handler = (event, context, callback) => {
     const alexa = Alexa.handler(event, context);
     alexa.appId = 'amzn1.ask.skill.fa292120-c0ab-439d-9b61-60bea0b14abd';
-    alexa.dynamoDBTableName = 'hitch';
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
@@ -21,5 +20,9 @@ exports.handler = (event, context, callback) => {
 const handlers = {
     'LaunchRequest' () {
         this.emit(':ask', speechOutput.WELCOME_MESSAGE);
+    },
+    'SendMessageIntent' (events) {
+      this.response.speak("Message sent.");
+      this.emit(':responseReady');
     }
 };
